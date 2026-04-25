cbba42b78cfb00b0580eda589b8594023fe4b717
vatorresra <vatorresra@unal.edu.co>
2026-04-25 13:48:59 -0500

    Refactor Station class to include bike removal method

--- Station.java
+++ Station.java
@@ -3,12 +3,13 @@ public class Station {
     private String name_station;
     private int maxBicicletas;
     private int bicicletasAlmacenadas=0;
+    private Bike [] bicis;
 
     //Constructor
     public Station (String name_station, int maxBicicletas){
         this.name_station=name_station;
         this.maxBicicletas=maxBicicletas;
-        Bike [] bicis=new Bike [maxBicicletas];
+        this.bicis=new Bike [maxBicicletas];
     }
 
     public boolean agregarBicicleta(Bike bicicleta){
@@ -29,6 +30,37 @@ public class Station {
         return "La bicicleta" + bicicleta.getId() + "NO se encuentra aquí." + "\n" + "Busque en otra estación.";
     }
 
+    public boolean retirarBicicleta(Bike bicicleta){
+        for (int i=0;i<bicicletasAlmacenadas;i++) {
+            if (bicis[i].getId()==bicicleta.getId()){
+                bicis[i]=null;
+                bicicletasAlmacenadas--;
+                return true;
+            }
+        }
+        return false;
+    }
 
+    public String getName_station() {
+        return name_station;
+    }
+    
+    public int getMaxBicicletas() {
+        return maxBicicletas;
+    }
+
+    public int getBicicletasAlmacenadas() {
+        return bicicletasAlmacenadas;
+    }
+
+    public Bike[] getBicis() {
+        return bicis;
+    }
+    
+    public void info(){
+        System.out.println("Nombre de la estación: " + name_station);
+        System.out.println("Capacidad máxima de bicicletas: " + maxBicicletas);
+        System.out.println("Número de bicicletas almacenadas: " + bicicletasAlmacenadas);
+    }
     
 }
