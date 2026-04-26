a3be23f4f725b20420d958b0f9344ed2826d16f5
xmalagon-ctrl <xmalagon@unal.edu.co>
2026-04-25 23:49:46 -0500

    Update Administrator.java

--- Administrator.java
+++ Administrator.java
@@ -1,4 +1,7 @@
 //clase hija para administraador
+
+import java.util.ArrayList;
+
 public class Administrator extends User{
     //atributos
     //..
@@ -9,9 +12,21 @@ public class Administrator extends User{
     }
 
     //metodos
-   // public void penalizeStudent(Student listaEstudiante, ){
+   public boolean penalizeStudent(ArrayList<Student> listaEstudiante, long tiun, String estado){
+        for (int i = 0; i < listaEstudiante.size(); i++){
+            if(listaEstudiante.get(i).getTiun() == tiun){
+                if(listaEstudiante.get(i).setState(estado)){
+                    System.out.println("Estado actualizado correctamente.");
+                    return true;
+                }else{
+                    return false;
+                }
+            }
+        }
 
-  //  }
+        System.out.println("Ese TIUN no pertenece a ningún estudiante");
+        return false;
+    }
    //addBicycle(), viewReports(), removeStudent()*/
 
 }
