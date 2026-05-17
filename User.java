d681d294c1d93585129e9f0b0b943b351fffc651
xmalagon-ctrl <xmalagon@unal.edu.co>
2026-05-16 22:46:25 -0500

    Update User.java

--- User.java
+++ User.java
@@ -1,8 +1,8 @@
 //clase madre user
 public abstract class User {
     //Atributos
-    private String username;
-    private long cedula; 
+    private String username, correo;
+    private long cedula, numPersonal; 
 
     //Constructor
     public User (String username, long cedula){
@@ -16,6 +16,13 @@ public abstract class User {
 
     public long getCedula(){
         return cedula;
+    }
+    public String getCorreo(){
+        return correo;
+    }
+
+    public long getNumPersonal(){
+        return numPersonal;
     }
 
     //Set
@@ -42,6 +49,28 @@ public abstract class User {
             return false;   
         }
     }
+    //verificacion de que ingrese un valor de correo correcto.
+    public boolean setCorreo(String correo){
+        if (correo!= null && !correo.isEmpty()){   
+            this.correo =correo;
+            return true;      
+        }else {
+            System.out.println("Correo invalido");
+            return false;
+        }
+    }
+    //verificacion de numero personal
+    public boolean setNumPersonal(long numPersonal){
+        String numPersonalString =String.valueOf(numPersonal);
+        if (numPersonalString.length() == 10  ){  
+            this.numPersonal = numPersonal;
+            return true;
+        }else {
+            System.out.println("Numero personal invalido");
+            return false;   
+        }
+    }
+
 
     //Metodos
     public void seeRules(){ 
