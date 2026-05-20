3f14981ea9f2493409222befbcfc749993a3c547
xmalagon-ctrl <xmalagon@unal.edu.co>
2026-05-19 22:33:52 -0500

    Refactor penalizeStudent method in Administrator class

--- Administrator.java
+++ Administrator.java
@@ -1,32 +1,44 @@
 //clase hija para administraador
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 public class Administrator extends User{
     //atributos
-    //..
+
 
     //constructor
     public Administrator (String username, long cedula){
         super(username, cedula);
     }
 
+
     //metodos
-   public boolean penalizeStudent(ArrayList<Student> listaEstudiante, long tiun, String estado){
-        for (int i = 0; i < listaEstudiante.size(); i++){
-            if(listaEstudiante.get(i).getTiun() == tiun){
-                if(listaEstudiante.get(i).setState(estado)){
-                    System.out.println("Estado actualizado correctamente.");
+    // En la clase Administrator (Ya no necesita recibir el objeto Reservar)
+    public boolean penalizeStudent(ArrayList<Student> listaEstudiante, long tiun) {
+        
+        for (Student student : listaEstudiante) {
+            if (student.getTiun() == tiun) {
+                // El administrador modifica directamente el estado del estudiante encontrado
+                if(student.getContadorPenalizaciones() >= 2){//esta es por si cuenta con 2 o mas penalizaciones
+                    student.setFechaFinPenalizacion(LocalDateTime.now().plusDays(30));
+                    student.setState("bloqueado");
+                    System.out.println("PENALIZACIÓN MANUAL APLICADA POR EL ADMINISTRADOR.");
+                    System.out.println("Cuenta con mas de una penalizacion. \nSe bloqueara por 30 dias"); 
                     return true;
                 }else{
-                    return false;
-                }
+                    student.setFechaFinPenalizacion(LocalDateTime.now().plusDays(15));
+                    student.setState("bloqueado");
+                    System.out.println("PENALIZACIÓN MANUAL APLICADA POR EL ADMINISTRADOR.");
+                    System.out.println("El estudiante " + student.getUserName() + " ha sido bloqueado por 15 días.");
+                    student.setContadorPenalizaciones();
+                    return true;
+                }   
             }
         }
-
-        System.out.println("Ese TIUN no pertenece a ningún estudiante");
+        System.out.println("Ese TIUN no pertenece a ningún estudiante registrado.");
         return false;
     }
-   //addBicycle(), viewReports(), removeStudent()*/
 
-}
+
+   //addBicycle(), viewReports(), removeStudent()*/
