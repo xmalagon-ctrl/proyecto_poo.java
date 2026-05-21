aa95fadca0b19ac114f31c768d8af03fabf31aba
xmalagon-ctrl <xmalagon@unal.edu.co>
2026-05-21 17:36:56 -0500

    Update Bike.java

--- Bike.java
+++ Bike.java
@@ -1,79 +1,70 @@
 public class Bike {
     
-        public int getId() {
-        return id;
-    }
-
-        public String getState() {
-        return state;
-    }
-
-
     //Atributos
     private String state = "disponible";
     private int id = 0; 
 
-    //cosjtructor
-        public Bike(int id, String state) {
-            this.id = id;
-            this.state = "disponible";
-        }
+    //constructor
+    public Bike(int id, String state) {
+        this.id = id;
+        this.state = state;
+    }
 
-        //met reserva
-//state reservada
+    //get
+    public int getId() {
+        return id;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+
+    //Metodos
+
+    //met reserva
+    //state reservada
     public boolean reservar(){
-
 
         if (state.equals("disponible")){
             state = "reservada";
-            
-
             return true;
         }else{
-
-            
             return false;
         }
     }
+    public void desReservar(){
 
-
-    
-//state disponible
+        if (state.equals("reservada")){
+            state = "disponible";
+            //return true;
+        }else{
+            //return false;
+        }
+    }
+        
     public  void disponible() {
-
         state = "disponible";
         System.out.println("bicicleta " + id + "está disponible");
-
     }
-//no
+    
     public  void No_disponible() {
-
         state = "mantenimiento";
         System.out.println("bicicleta " + id + "está en mantenimiento");
+    }
 
+    public boolean cambiarEstado(String newState){
+
+        if(newState.equals("disponible")){
+            disponible();
+            return true;
+        }else if(newState.equals("mantenimiento")){
+            No_disponible();
+            return true;
+        }
+        System.out.println("Estado inválido.");
+        return false;
     }
 
 
-    // informacion de la bicicleta
-    public void info (){
-
-        System.out.println("id: " + id);
-        System.out.println("estado: " + state);
-    }
-
-//metodo para cambiar el estado
-
-public boolean cambiarEstado(String newState){
-
-    if(newState.equals("disponible")){
-        disponible();
-        return true;
-    }else if(newState.equals("mantenimiento")){
-        No_disponible();
-        return true;
-    }
-    System.out.println("Estado inválido.");
-    return false;
 }
-}
-
